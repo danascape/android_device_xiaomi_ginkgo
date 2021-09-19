e50b13c62bf99ccfda65606f7e1349bb9140ebb5
Saalim Quadri <danascape@gmail.com>
2021-09-19 22:36:11 +0530

    g4w4: Build neural networks 1.2 HAL

--- device-hidl.mk
+++ device-hidl.mk
@@ -37,6 +37,7 @@ PRODUCT_PACKAGES += \
 
 # NeuralNetworks
 PRODUCT_PACKAGES += \
+    android.hardware.neuralnetworks@1.2.vendor \
     android.hardware.neuralnetworks@1.1.vendor \
     android.hardware.neuralnetworks@1.0.vendor
 
