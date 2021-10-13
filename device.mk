81506ea7507520cd5712c49837e1f4354551da40
danascape <danascape@gmail.com>
2021-10-13 18:39:27 +0200

    g4w4: Drop system variant of libvndfwk_detect_jni
    * apex android art opens the lib but expects it in vendor-classloader-namespace
    
    Test: Boot

--- device.mk
+++ device.mk
@@ -202,7 +202,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libqti_vndfwk_detect \
     libqti_vndfwk_detect.vendor \
-    libvndfwk_detect_jni.qti \
     libvndfwk_detect_jni.qti.vendor
 
 # GPS
